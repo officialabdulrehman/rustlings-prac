a682bdeba6dd9714147dfda0253bf2ff25b4fe52
NizTheDev <arehman08@hotmail.com>
2022-11-02 23:05:23 +0500

    feat: compeleted => hashmaps

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,8 +14,6 @@
 
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 // A structure to store team name and its goal details.
@@ -40,6 +38,53 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+
+        // SOLUTION 1
+        // let mut team1 = scores
+        //     .entry(team_1_name)
+        //     .or_insert_with_key(|team_name| Team {
+        //         name: team_name.to_string(),
+        //         goals_scored: 0,
+        //         goals_conceded: 0,
+        //     });
+        // team1.goals_scored += team_1_score;
+        // team1.goals_conceded += team_2_score;
+        // let mut team2 = scores
+        //     .entry(team_2_name)
+        //     .or_insert_with_key(|team_name| Team {
+        //         name: team_name.to_string(),
+        //         goals_scored: 0,
+        //         goals_conceded: 0,
+        //     });
+        // team2.goals_scored += team_2_score;
+        // team2.goals_conceded += team_1_score;
+
+        // SOLUTION 2
+
+        scores
+            .entry(team_1_name.clone()) //cloned it to be used later in a more ergonomic way, since "entry" expects a "String" and not "&String"
+            .and_modify(|t| {
+                //"t" being the value of the "scores: HashMap<String, Team>" which is a team type.
+                t.goals_scored += team_1_score; // no need for deref on "t" (like, *t), since t is a u8
+                t.goals_conceded += team_2_score;
+            })
+            .or_insert(Team {
+                name: team_1_name,
+                goals_scored: team_1_score,
+                goals_conceded: team_2_score,
+            });
+
+        scores
+            .entry(team_2_name.clone()) // just the same implementation but for team 2
+            .and_modify(|t| {
+                t.goals_scored += team_2_score;
+                t.goals_conceded += team_1_score;
+            })
+            .or_insert(Team {
+                name: team_2_name,
+                goals_scored: team_2_score,
+                goals_conceded: team_1_score,
+            });
     }
     scores
 }
