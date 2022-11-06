0e4117b13700e97caf94506ffa5b994278e99fe6
NizTheDev <arehman08@hotmail.com>
2022-11-06 23:55:50 +0500

    feat: completed conversions. [ The End ] :: Time for Recap and detailed book reading. { Gotta catch crabby }

--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -9,6 +9,8 @@
 use std::num::ParseIntError;
 use std::str::FromStr;
 
+use ParsePersonError::{BadLen, Empty, NoName, ParseInt};
+
 #[derive(Debug, PartialEq)]
 struct Person {
     name: String,
@@ -28,8 +30,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
-
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
 // 2. Split the given string on the commas present in it
@@ -46,6 +46,24 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.is_empty() {
+            Err(Empty) // i.e., Err(ParsePersonError::Empty)
+        } else {
+            let p: Vec<&str> = s.split(',').collect();
+            if p.len() != 2 {
+                Err(BadLen)
+            } else if p[0].len() == 0 {
+                Err(NoName)
+            } else {
+                match p[1].parse::<usize>() {
+                    Ok(a) => Ok(Person {
+                        name: p[0].to_string(),
+                        age: a,
+                    }),
+                    Err(a) => Err(ParseInt(a)),
+                }
+            }
+        }
     }
 }
 
